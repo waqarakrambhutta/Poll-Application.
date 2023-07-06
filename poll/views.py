92e2097fc97c7808b072ed485f1cfef4dd9b7171
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-07-06 17:37:11 +0500

    Handling get method error

--- poll/views.py
+++ poll/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
 from .models import Question
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('poll/index.html')
-    # template = loader.get_template('poll/index.html')
-
-
     return render(request,'poll/index.html',{'latest_question_list':latest_question_list})
 
 def detail(request,question_id):
-    return HttpResponse('You are looking at question %s.'%question_id)
+    try:
+        question  = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist.')
+    return render(request,'poll/index.html',{'question':question})
 
 def results(request, question_id):
     response = 'You are looking at the results question %s'
