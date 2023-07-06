1aa7d3c33ef34fb23073c39f81df5ecedeb1c462
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-07-06 15:15:21 +0500

    Mapping Urls

--- poll/urls.py
+++ poll/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
+    path('<int:question_id>/',views.detail,name='details'),
+    path('<int:question_id>/results/',views.results,name='results'),
+    path('<int:question_id/vote>',views.vote,name='vote')
 ]
