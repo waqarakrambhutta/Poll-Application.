2221ae8deb0243aa7f3c36017177429b815981db
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-07-06 14:43:40 +0500

    Adding some function to model

--- poll/admin.py
+++ poll/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Question,Choice
 
 # Register your models here.
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display= ['question_text']
