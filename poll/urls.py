d863e791570216b4bc75bd3d70c31ede1778d59a
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-07-09 14:29:18 +0500

    adding pk in urls

--- poll/urls.py
+++ poll/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name='poll'
 urlpatterns = [
     path('',views.IndexView.as_view()),
-    path('<int:question_id>/',views.DetailView.as_view(),name='detail'),
-    path('<int:question_id>/results/',views.ResultView.as_view(),name='result'),
-    path('<int:question_id>/vote/',views.VoteView.as_view(),name='vote')
+    path('<int:pk>/',views.DetailView.as_view(),name='detail'),
+    path('<int:pk>/results/',views.ResultView.as_view(),name='result'),
+    path('<int:question_id>/vote/',views.vote,name='vote')
 ]
--- poll/views.py
+++ poll/views.py
@@ -26,6 +26,20 @@ class ResultView(generic.DetailView):
     template_name = 'poll/results.html'
 
 
-class VoteView(generic.DetailView):
-    model = Question
-    template_name = 'poll/results.html'
+def vote(request,question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request,
+               'poll/detail.html',
+               {
+                   'question':question,
+                   'error_message':"You didn't select a choice."
+               }
+               )
+
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('poll:result',args=(question_id)))
